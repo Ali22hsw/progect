18ed798de232a6800679f37479baa9795f4a153a
Ali <Ali@A.gmail.com>
2024-03-27 07:07:07 +0300

    اظافة الداش بورد

---
+++ DashBorad/orders.js
@@ -0,0 +1,33 @@
+// نظوري هاي الصفحة نكتب بيها الشغلات الراح تنعرض بلجدوال
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'prnding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'prnding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'prnding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'prnding'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'prnding'
+    }
+]
