55c928f2cc8d96555e24d875461c0b9a8646e76c
Mohammad Zohar <mohammam.mz@gmail.com>
2024-03-30 17:35:52 +0300

    removed password

--- in/show.php
+++ in/show.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 	$username = "root";
-	$password = "root";
+	$password = "";
 	$dbname = "complaints";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
